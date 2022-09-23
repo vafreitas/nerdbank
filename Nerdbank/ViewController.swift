6e163d80f445069b6392d880746039bc5ef18b73
Victor Freitas <tqi_valves@uolinc.com>
2022-09-23 17:02:05 -0300

    Initial Commit

---
+++ Nerdbank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nerdbank
+//
+//  Created by Victor Alves de Freitas on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
